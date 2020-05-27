db1582bbb169d0d28570725428c56d412828fed2
kouhei <kouhei19951112@gmail.com>
2020-05-27 04:10:20 +0000

    Update Gemfile for Heroku

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,27 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
+  before_action :set_user, only: [:show, :edit, :update, :destroy, :edit_basic_info, :update_basic_info ]
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: [:destroy, :edit_basic_info, :update_basic_info]
   before_action :set_one_month, only: :show
-
+  before_action :admin_or_correct_user, only: :show
+  
+  
+  
   def index
-    @users = User.paginate(page: params[:page])
+    @users = User.paginate(page: params[:page]).search(params[:search])
+    if params[:search].present?
+    end
+    unless current_user?(@user) || current_user.admin?
+      flash[:danger] = "閲覧権限がありません。"
+      redirect_to(root_url)
+    end
   end
 
   def show
-  @worked_sum = @attendances.where.not(started_at: nil).count
+    @worked_sum = @attendances.where.not(started_at: nil).count
   end
+
 
   def new
     @user = User.new
@@ -65,6 +75,18 @@ class UsersController < ApplicationController
     end
 
     def basic_info_params
-      params.require(:user).permit(:department, :basic_time, :work_time)
+      params.require(:user).permit(:basic_time, :work_time)
+    end
+    
+    
+     # beforeフィルター
+    
+    # 管理権限者、または現在ログインしているユーザーを許可します。
+    def admin_or_correct_user
+      @user = User.find(params[:user_id]) if @user.blank?
+      unless current_user?(@user) || current_user.admin?
+        flash[:danger] = "編集権限がありません"
+        redirect_to(root_url)
+      end
     end
 end
